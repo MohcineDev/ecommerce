32d11a7c218569a19442ba33922c6c4158861c71
MohcineDev <mouhcinedev@gmail.com>
2022-05-08 22:43:38 +0100

    style single product

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,12 +1,26 @@
+import Head from "next/head"
+import Image from "next/image"
+import styles from '../../styles/singleProduct.module.css'
 
 export default function SingleProduct(props) {
-    const {title, price, description, category, image} = props.product
-    return (
-        <div className="container">
+    const { title, price, description, category, image } = props.product
 
-            {
-                props.product.title
-            }
+    return (
+        <div className={`${styles.product} container`}>
+            <Head>
+                <title>{title}</title>
+            </Head>
+
+            <div>
+                <Image src={image} width="300" height="300" />
+            </div>
+            <div>
+                <h2>{title}</h2>
+                <h3>{category}</h3>
+                <h3>{price}$</h3>
+                <p>{description}</p>
+            </div>
+
         </div>
     )
 }
